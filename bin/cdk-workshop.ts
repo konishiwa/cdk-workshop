c338084ec3098e7a82ca0b6292dfe26f581246fa
Konishi, Ruka <RKonishi@cvent.com>
2020-06-03 13:18:27 -0400

    fix: add workshop files

--- bin/cdk-workshop.ts
+++ bin/cdk-workshop.ts
@@ -3,4 +3,5 @@ import * as cdk from '@aws-cdk/core';
 import { CdkWorkshopStack } from '../lib/cdk-workshop-stack';
 
 const app = new cdk.App();
+// instantiates the CdkWorkshopStack class from the lib/cdk-workshop-stack.ts
 new CdkWorkshopStack(app, 'CdkWorkshopStack');
